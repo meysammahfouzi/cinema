44f88b36f208ea6e263529e67e069fa613afa750
Meysam Mahfouzi <meysammahfouzi@users.noreply.github.com>
2016-11-05 15:58:18 +0330

    Fixed pylint warnings

--- filmRank.py
+++ filmRank.py
@@ -1,44 +1,61 @@
+"""
+This module provides functionality to retrieve
+the rating of a movie from IMDB.
+"""
+
 from urllib import FancyURLopener
 import urllib
 import re
 from bs4 import BeautifulSoup
 
-import sys
-
 
 class MyOpener(FancyURLopener, object):
-    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+    """
+    This class helps in sending query to HTTPS pages
+    """
+    version = "Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) " \
+              "Gecko/20071127 Firefox/2.0.0.11"
 
 
-def getRating(filmName):
-    params = urllib.urlencode({'q': filmName + ' imdb'})
+def get_rating(film_name):
+    """
+    Retrieves the rating of a movie by its name
+    :param film_name: Name of the movie
+    :return: Its IMDB rating
+    """
+    params = urllib.urlencode({'q': film_name + ' imdb'})
     opener = MyOpener()
     result = ""
     try:
         socket = opener.open('https://www.google.com/search?{}'.format(params))
     except IOError:
-        print("no connection available!")
+        print "no connection available!"
     else:
         result = socket.read()
 
-    list = getListofFilms(result)
+    first_index = result.find("Rating:")
 
-    firstIndex = result.find("Rating:")
+    rating_index = first_index + len("Rating:")
+    rating = result[rating_index:rating_index + 10].strip()
+    pattern = re.compile(r"\d{1}\.?\d{0,2}/10")
+    matched = pattern.match(rating)
 
-    ratingIndex = firstIndex + len("Rating:")
-    rating = result[ratingIndex:ratingIndex + 10].strip()
-    p = re.compile(r"\d{1}\.?\d{0,2}/10")
-    m = p.match(rating)
-
-    if m:
-        # print("rating: {}".format(m.group()))
-        return m.group()
+    if matched:
+        # print "rating: {}".format(matched.group())
+        return matched.group()
     else:
-        print("no rating found for {}".format(filmName))
+        print "no rating found for {}".format(film_name)
         return 0
 
-def getListofFilms(html):
+
+def __get_list_of_films(html):
+    """
+    Returns list of films available in the HTML provided
+    :param html: The HTML string containing list of movies
+    :return: List of movie titles
+    """
     soup = BeautifulSoup(html, "html.parser")
-    imdbLinks = soup.select("div.slp")
-    # return [h3.text for h3 in imdbLinks if re.search('imdb', h3.text, re.IGNORECASE)]
-    return [h3.text for h3 in imdbLinks]
+    imdb_links = soup.select("div.slp")
+    # return [h3.text for h3 in imdb_links
+    #         if re.search('imdb', h3.text, re.IGNORECASE)]
+    return [h3.text for h3 in imdb_links]
--- test_rating.py
+++ test_rating.py
@@ -1,15 +1,18 @@
 """This module contains tests for retrieving movie ratings."""
 
-from filmRank import getRating
+from filmRank import get_rating
+
 
 def test_1():
     """ Tests the rating of the movie called 'A Separation' """
-    assert getRating("separation") == '8.4/10'
+    assert get_rating("separation") == '8.4/10'
+
 
 def test_2():
     """ Tests the rating of the movie called 'Shawshank Redemption' """
-    assert getRating("shawshank redemption") == '9.3/10'
+    assert get_rating("shawshank redemption") == '9.3/10'
+
 
 def test_3():
     """ Tests the rating of the movie called 'Sixth Sense' """
-    assert getRating("sixth sense") == '8.1/10'
+    assert get_rating("sixth sense") == '8.1/10'
