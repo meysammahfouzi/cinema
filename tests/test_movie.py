b65fcc5a1801627269378ccc645464ab7ab97a90
meysam <meysammahfouzi@users.noreply.github.com>
2016-11-14 15:15:22 +0330

    Updated unit tests

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -11,14 +11,51 @@ class TestMovie(TestCase):
     def test_movie_director(self):
         self.assertEqual(Movie('A Separation').directors[0], 'Asghar Farhadi')
 
-    def test_imdb_rating(self):
+    def test_imdb_rating_1(self):
         self.assertEqual(Movie('shawshank redemption').imdb_rating, 9.3)
+
+    def test_imdb_rating_2(self):
+        self.assertEqual(Movie("The Lord of the Rings", exact_match=False, year=2001).imdb_rating, 8.8)
+
+    def test_imdb_rating_3(self):
+        self.assertEqual(Movie("The Lord of the Rings", exact_match=False, year=2002).imdb_rating, 8.7)
+
+    def test_imdb_rating_4(self):
+        self.assertEqual(Movie("The Lord of the Rings", exact_match=False, year=2003).imdb_rating, 8.9)
+
+    def test_imdb_rating_5(self):
+        self.assertEqual(Movie("The Lord of the Rings", exact_match=False, year=1978).imdb_rating, 6.2)
 
     def test_movie_year(self):
         self.assertEqual(Movie('sixth sense').year, 1999)
 
     def test_movie_cast(self):
-        self.assertEqual(", ".join(Movie('matrix').cast), "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving")
+        self.assertEqual(", ".join(Movie('matrix').cast),
+                         "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving")
 
-    def test_movie_cast(self):
+    def test_movie_imdb_id(self):
         self.assertEqual(Movie('match point').imdb_id, 'tt0416320')
+
+    def test_movie_genre(self):
+        self.assertEqual(", ".join(Movie('godfather').genre), 'Crime, Drama')
+
+    def test_movie_awards(self):
+        self.assertEqual(Movie('12 Angry Men').awards, 'Nominated for 3 Oscars. Another 16 wins & 8 nominations.')
+
+    def test_movie_country(self):
+        self.assertEqual(", ".join(Movie("schindler's list").country), 'USA')
+
+    def test_movie_language(self):
+        self.assertEqual(", ".join(Movie("pulp fiction").language), 'English, Spanish, French')
+
+    def test_movie_writers(self):
+        self.assertEqual(", ".join(Movie("fight club").writers), 'Chuck Palahniuk (novel), Jim Uhls (screenplay)')
+
+    def test_movie_tomato_meter(self):
+        self.assertEqual(Movie("star wars", exact_match=False, year=1980).tomatometer, 94)
+
+    def test_movie_tomato_plot(self):
+        self.assertEqual(Movie("Forrest Gump").plot,
+                         "Forrest Gump, while not intelligent, has accidentally been "
+                         "present at many historic moments, but his true love, Jenny "
+                         "Curran, eludes him.")
