0d247afc19d3841717660276cc60d77813ec57cf
Meysam Mahfouzi <maysam.mahfouzi@gmail.com>
2016-11-20 01:18:53 +0330

    0.0.4

--- cinema/__init__.py
+++ cinema/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'cinema'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __build__ = __version__
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup
 setup(
     name='cinema',
     packages=['cinema'],
-    version='0.0.3',
+    version='0.0.4',
     description='Everything about Cinema!',
     author='Meysam Mahfouzi, Majid Hajiloo',
     author_email='maysam.mahfouzi@gmail.com, majid.hajiloo@gmail.com',
     url='https://github.com/meysammahfouzi/cinema',
-    download_url = 'https://github.com/meysammahfouzi/cinema/tarball/0.0.3',
+    download_url = 'https://github.com/meysammahfouzi/cinema/tarball/0.0.4',
     license='MIT',
     keywords=['movie', 'rating', 'imdb', 'film', 'cinema', 'actor', 'metascore', 'tomatometer'],
     install_requires=[
