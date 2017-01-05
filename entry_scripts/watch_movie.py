17072e76f999d87e2701cf910326025781f0d1d8
Mohammad Torkashvand <m.torkashvand2009@gmail.com>
2017-01-05 16:55:29 +0330

    Some improvement in command line arguments

--- entry_scripts/watch_movie.py
+++ entry_scripts/watch_movie.py
@@ -7,23 +7,16 @@ from cinema.movie import Movie, MovieNotFound
 
 def main():
     """The main routine."""
-    parser = argparse.ArgumentParser(usage="usage: %prog [options]",
-                                     description='This python scripts retrieves the rating of a movie with its title',
-                                     prog='watch_movie')
-    parser.add_argument("-n", "--name", help="The name of movie you want to search", type=str)
-    parser.add_argument("--ry", "--release_year", type=int, help='The release year of the movie.', default=0)
-    parser.add_argument("--em", "--exact_match", action="store_true", default=False,
-                        help="search exact name of the movie.")
+    parser = argparse.ArgumentParser(
+        description="This python scripts retrieves the rating of a movie with its title. \n\texample: \n\t>> watch-movie 'lord of the rings' 2003")
+    parser.add_argument("name", help="The name of movie you want to search")
+    parser.add_argument("release_year", type=int, help='The release year of the movie.', nargs='?', default=0)
+    parser.add_argument("-e", "--exact_match", action="store_true", help="search exact name of the movie.")
 
     args = parser.parse_args()
 
-    if len(args) < 1:
-        parser.error("Type 'watch-movie.py --help <option>' for help on a specific option."
-                     "\nUsage:\n \twatch-movie.py --name --release_year\n"
-                     "\texample: \n\twatch-movie.py -n 'lord of the rings' -ry 2003\n")
-
     try:
-        movie = Movie(name=args.name.strip(), exact_match=args.exact_match, year=args.release_year)
+        movie = Movie(name=args.name, exact_match=args.exact_match, year=args.release_year)
         print("{} ({})".format(movie.title, movie.year))
         print("Awards: {}".format(movie.awards))
         print("IMDB Rating: {}/10".format(movie.imdb_rating))
